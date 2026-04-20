01fa13f8c162afadade1b3754d1e212811d60eab
stuti5verma <stutiverma.aj@gmail.com>
2026-04-20 14:10:35 +0530

    UC5: Validate user move

--- src/com/tictactoe/Main.java
+++ src/com/tictactoe/Main.java
@@ -1,17 +1,30 @@
 public class Main {
 
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
     public static void main(String[] args) {
-        int slot = 7;
 
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        System.out.println(isValidMove(1, 1)); // true
+
+        board[1][1] = 'X';
+
+        System.out.println(isValidMove(1, 1)); // false
     }
 
-    static int getRowFromSlot(int slot) {
-        return (slot - 1) / 3;
-    }
+    static boolean isValidMove(int row, int col) {
 
-    static int getColFromSlot(int slot) {
-        return (slot - 1) % 3;
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
     }
 }
